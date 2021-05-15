2fef786209f4a26c8f6bc7558217477bd11d2006
Márcio Bernardes <marciobernardes@live.com>
2021-05-15 19:22:13 -0300

    log enhancements

--- application/use_cases/upwork/tasks.py
+++ application/use_cases/upwork/tasks.py
@@ -67,7 +67,7 @@ def scan_upwork(self, user_credentials):
     
     except WebDriverException as exc:
         logger.exception(
-            "unexpected webdriver error while connecting with the host, will NOT retry")
+            "unexpected webdriver error while connecting with the host, will not retry. error %s", repr(exc))
         raise 
 
     except Exception as exc:
